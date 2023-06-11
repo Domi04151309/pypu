3db5b16d2215d7af231cf9f5ac5e3e344adb887f
domi04151309 <dominikworldlp@gmail.com>
2023-06-11 05:44:37 +0200

    Remove unused method

--- data/SourceFile.py
+++ data/SourceFile.py
@@ -15,22 +15,6 @@ class SourceFile:
         self.functions: list[SourceFunction] = []
         self.classes: list[SourceClass] = []
 
-    def add_class_variable(
-            self, source_class: str,
-            variable: SourceVariable
-    ) -> None:
-        """
-        Appends a variable to a source class.
-
-        :param source_class: The class to append the variable to.
-        :param variable: The variable to append.
-        :return: Nothing.
-        """
-        for class_item in self.classes:
-            if class_item.name == source_class:
-                class_item.variables.append(variable)
-                return
-
     def get_connection_strings(self) -> str:
         """
         Generates a valid PlantUML string for class associations.
